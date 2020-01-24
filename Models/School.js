aac6d6ad38a3b0157ca07fdd23f0c66eafc17ee9
Dinuka Shameera <dinukashameera0657@gmail.com>
2020-01-24 23:49:04 +0530

    creating school model

---
+++ Models/School.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+
+const schoolSchema = new mongoose.Schema({
+    schoolName : String,
+    province : String,
+    district : String    
+});
+
+const School = mongoose.model('School','schoolSchema')
+
+exports.School = School;
